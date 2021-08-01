0ede762619dbc0f7836f22da1e66ec4f7f94e943
Hernán GB <hernanbertolini@gmail.com>
2021-08-01 20:26:22 -0300

    TestBase

--- src/test/java/testNGTests/Test1.java
+++ src/test/java/testNGTests/Test1.java
@@ -4,15 +4,18 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.testng.annotations.Test;
 import org.testng.asserts.SoftAssert;
+
+import com.aventstack.extentreports.Status;
+
 import pages.Page1;
 
 public class Test1 extends TestBase{
 //	public static WebDriver driver=null;
 	
-	private static Logger log=LogManager.getLogger(Test1.class.getName());
+//	private static Logger log=LogManager.getLogger(Test1.class.getName());
 	
 
-	@Test(enabled=false)
+	@Test(enabled=true)
 	public void testcase1() {
 		SoftAssert sassert = new SoftAssert();
 		String searchText = "testing Google search";
@@ -21,9 +24,10 @@ public class Test1 extends TestBase{
 		Page1 page = new Page1(driver.get());
 		page.searchText(driver.get(), searchText);	
 		sassert.assertEquals(page.getTitle(driver.get()), searchText + " - Buscar con Google");
-		log.error("test error message");
-		log.info("completed step");
-		log.fatal("test fatal error");
+		extentTest.get().log(Status.PASS, "Test1 PASSED");
+//		log.error("test error message");
+//		log.info("completed step");
+//		log.fatal("test fatal error");
 		sassert.assertAll();
 	}
 
--- src/test/java/testNGTests/Test2.java
+++ src/test/java/testNGTests/Test2.java
@@ -4,6 +4,8 @@ package testNGTests;
 import org.testng.annotations.Test;
 import org.testng.asserts.SoftAssert;
 
+import com.aventstack.extentreports.Status;
+
 public class Test2 extends TestBase {
 
 	@Test
@@ -11,6 +13,7 @@ public class Test2 extends TestBase {
 		SoftAssert sassert = new SoftAssert();
 		System.out.println("test2");
 		driver.get().get("https://test.salesforce.com");
+		extentTest.get().log(Status.PASS, "Test2 PASSED");
 		sassert.assertAll();
 	}
 }
